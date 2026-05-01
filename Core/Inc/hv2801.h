4b8e6b06eb880ea0e778dea379fe98b23add2814
matianfu <matianfu@gingerologist.com>
2026-05-01 20:39:09 +0800

    clean and reformat

--- Core/Inc/hv2801.h
+++ Core/Inc/hv2801.h
@@ -8,6 +8,11 @@
 #ifndef INC_HV2801_H_
 #define INC_HV2801_H_
 
+/**
+ * This file is not relevant anymore since the analog switch (HV2801)
+ * has been moved to another board (b).
+ */
+
 void HV2801_Init(void);
 void HV2801_CLR(void);
 void HV2801_SW(int index);
