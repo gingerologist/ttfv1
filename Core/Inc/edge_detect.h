ec69557862ddd847f3d4c58d811ad80ffca4e67b
matianfu <matianfu@gingerologist.com>
2026-05-01 23:56:55 +0800

    edge_detect reviewed

--- Core/Inc/edge_detect.h
+++ Core/Inc/edge_detect.h
@@ -12,35 +12,19 @@
 
 #include "stm32f4xx_hal.h"
 
-//typedef enum {
-//    SW_RELEASED,
-//    SW_DEBOUNCING_DOWN,
-//    SW_PRESSED,
-//    SW_DEBOUNCING_UP
-//} SW_Status;
-//
-//
-//
-//typedef struct {
-//    SW_Status status;
-//    uint8_t count;
-//    uint8_t edge;        // 0 = no edge, 1 = down edge, 2 = up edge
-//    uint8_t max_count;   // Configurable debounce threshold
-//} SW_HandleTypeDef;
-
 typedef enum {
-    SW_RELEASED,
-    SW_DEBOUNCING_DOWN,
-    SW_PRESSED,
-    SW_DEBOUNCING_UP
+  SW_RELEASED,
+  SW_DEBOUNCING_DOWN,
+  SW_PRESSED,
+  SW_DEBOUNCING_UP
 } SW_Status;
 
 typedef struct {
-    SW_Status status;
-    uint8_t count;
-    uint8_t max_count;   // Configurable debounce threshold
+  SW_Status status;
+  uint8_t count;
+  uint8_t max_count; // Configurable debounce threshold
 } SW_HandleTypeDef;
 
-int GPIO_EdgeDetect(SW_HandleTypeDef* handle, GPIO_PinState current_state);
+int GPIO_EdgeDetect(SW_HandleTypeDef *handle, GPIO_PinState current_state);
 
 #endif /* INC_EDGE_DETECT_H_ */
